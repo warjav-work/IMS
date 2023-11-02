f533e30f3fd766b5b71e43d76a73ee691393d88b
DESKTOP-UKDCDPL\usuario <warjav.work@gmail.com>
2023-11-02 15:56:03 +0100

    Agregar archivos de proyecto.

---
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,24 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.Inventories.Interfaces;
+using IMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.Inventories
+{
+    public class ViewInventoriesByNameUseCase : IViewInventoriesByNameUseCase
+    {
+        private readonly IInventoryRepository _inventoryRepository;
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            _inventoryRepository = inventoryRepository;
+        }
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await _inventoryRepository.GetInventoriesByNameAsync(name);
+        }
+    }
+}
