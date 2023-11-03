44dbfc5211f0dfdafccc31e2c0ddbd13ac6b3e8f
DESKTOP-UKDCDPL\usuario <warjav.work@gmail.com>
2023-11-03 09:44:58 +0100

    Agregar AddInventory

---
+++ IMS.UseCases/Inventories/Interfaces/AddInventoryUseCase.cs
@@ -0,0 +1,22 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Inventories.Interfaces
+{
+    public class AddInventoryUseCase : IAddInventoryUseCase
+    {
+        private readonly IInventoryRepository _inventoryRepository;
+
+        public AddInventoryUseCase(IInventoryRepository inventoryRepository)
+        {
+            _inventoryRepository = inventoryRepository;
+        }
+        public async Task ExecuteAsync(Inventory inventory)
+        {
+            if (!await _inventoryRepository.ExistsAsync(inventory))
+            {
+                await _inventoryRepository.AddInventoryAsync(inventory);
+            }
+        }
+    }
+}
--- IMS.UseCases/PluginInterfaces/IInventoryRepository.cs
+++ IMS.UseCases/PluginInterfaces/IInventoryRepository.cs
@@ -5,5 +5,7 @@ namespace IMS.UseCases.PluginInterfaces
     public interface IInventoryRepository
     {
         Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name);
+        Task<bool> ExistsAsync(Inventory inventory);
+        Task AddInventoryAsync(Inventory inventory);
     }
 }
